57d8330cb11045e0d58c5059fb1f18c011de3c5c
Bhanu <bhanu.vulloju@gmail.com>
2023-10-31 19:49:47 +0530

    cleanedup unwanted routes

--- app.py
+++ app.py
@@ -7,13 +7,13 @@ app = Flask(__name__, template_folder="templates")
 
 
 @app.route("/")
-def hello():
+def rootRoute():
     return redirect("/home")
 
 
 @app.route("/home")
 def homePage():
-    return render_template("home.html")
+    return render_template("home.html", response="")
 
 
 @app.route("/experiences")
@@ -29,56 +29,12 @@ def aboutPage():
 @app.route("/places", methods=['GET'])
 def placePage():
     placeName = request.args["query"]
-    print(placeName)
-    placeName = placeName.replace(' ', '+')
-    print(placeName)
 
     response = requests.get(
-        f'https://e4hw20a837.execute-api.us-east-1.amazonaws.com/v1/places?name={placeName}')
+        f'https://e4hw20a837.execute-api.us-east-1.amazonaws.com/v1/places?name={placeName}').json()
 
-    response = response.json()
-    placeName = placeName.replace('+', ' ')
 
-    print(response)
-    htmlContent = f'''
-    ~~~ &emsp;  ~~~ &emsp;  ~~~ &emsp; ~~~
-    <h1>  {placeName}</h1>
-    ~~~ &emsp;  ~~~ &emsp;  ~~~ &emsp; ~~~
-    <h2>🗺️{response['City']}, {response['State']}</h2>
-    <p></p>
-    <h3>{response['Description']}</h3>
-    <p></p>
-    
-    <div class="container">
-      <div class="row g-2">
-        <div class="col-6">
-          <div class="p-3 border bg-light">
-            <h4>⭐Rating - {response['Rating']}</h4>
-          </div>
-        </div>
-        <div class="col-6">
-          <div class="p-3 border bg-light">
-            <h4>⏱️Duration : {response['Duration']}</h4>
-          </div>
-        </div>
-        <div class="col-6">
-          <div class="p-3 border bg-light">
-            <h4>👨‍👩‍👧‍👦Visitors per year : {response['No of visitors per year']}</h4>
-          </div>
-        </div>
-        <div class="col-6">
-          <div class="p-3 border bg-light">
-            <h4>🏄‍♂️Best Time To Visit : {response['Best time to visit']}</h4>
-          </div>
-        </div>
-      </div>
-    </div>
-    <p></p>
-    <h4>💸Ticket fare : {response['Price']}</h4>
-    
-    '''
-    return render_template('home.html', cust_cont=htmlContent)
-    # return jsonify(data[placeName])
+    return render_template('places.html', response=response)
 
 
 
